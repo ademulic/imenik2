3176930ad288f6cea981ab6a2db2533f9b2f16a4
ademulic <ademulic@hotmail.com>
2024-11-05 19:33:40 +0100

    Zavrsen projekat

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ function App() {
   //   hobby: "", // input -> text
   // };
 
+  let [isIncluded, setIsIncluded] = useState(true);
+
   const [user, setUser]= useState({
     ime: "",  
     prezime: "", 
-    gender: "", 
-    age: "", 
+    gender: "male", 
+    age: "12-17", 
     number: "", 
     hobby: "" 
   }
@@ -25,8 +27,9 @@ function App() {
   const [userList,setUserList]=useState([]);
 
   const handleSubmit = (e)=>{
-    e.preventDefault();
-    if(user !== null){
+    
+    if(user !== null && !userList.includes(user.ime && user.prezime)){
+      setIsIncluded(false);
       userList.push(user)
       setUserList(userList);
       console.log(userList);
@@ -34,14 +37,15 @@ function App() {
         ime: "",  
         prezime: "", 
         gender: "", 
-        age: "", 
+        age: "12 - 17", 
         number: "", 
         hobby: "" 
       });
       
     }else{
-      return;
+      return
     }
+    e.preventDefault();
   }
 
   const handleInputChange = (event) => {
@@ -52,6 +56,9 @@ function App() {
   return (
     <div className="App">
        <aside className="editor">
+          <div style={{paddingTop:"70px"}}>
+            <h1 style={{fontSize:40}}>Imenik</h1>
+          </div>
           <form onSubmit={handleSubmit}>
             <h1>Add a Member</h1>
             <p>Please fill out the form</p>
@@ -64,32 +71,53 @@ function App() {
               <input type="text" placeholder="Enter your last name..." name="prezime" value={user.prezime} onChange={(event) => handleInputChange(event)} required/>  
             </div>
             <div className="radio-btns">
-              <input type="radio" name="gender" id="male"  value={"male"} onChange={(event) => handleInputChange(event)} required/>
+              <input type="radio" name="gender" id="male"  value={"male"} onChange={(event) => handleInputChange(event)}required/>
               <label htmlFor="male">Male: </label>
-              <input type="radio" name="gender" id="female" value={"female"} onChange={(event) => handleInputChange(event)} required/>
+              <input type="radio" name="gender" id="female" value={"female"} onChange={(event) => handleInputChange(event)}required/>
               <label htmlFor="female">Female: </label>
             </div>
-            <select name="age" id="age" onChange={(event) => handleInputChange(event)}>
-              <option value="12 - 17 " seleced = "true">12 - 17</option>
+            <select name="age" id="age" onChange={(event) => handleInputChange(event)} required>
+              <option value="12 - 17">12 - 17</option>
               <option value="18 - 33" >18 - 33</option>
               <option value="34 - 50" >33 - 50</option>
             </select>
+            <div>
+              <label htmlFor="phoneNumber">Phone Number: </label>
+              <input type="text" name="number" id="phoneNumber" placeholder="Enter your phone number..." value={user.number} onChange={(event) => handleInputChange(event)} required/>
+            </div>
             <div>
               <label htmlFor="hobby">Your Hobby: </label>
               <input type="text" name="hobby" id="hobby" placeholder="Enter your hobby..." value={user.hobby} onChange={(event) => handleInputChange(event)} required/>
             </div>
             <button type="submit">Add Member</button>
           </form>
+          <p>Created by Adem Mulic 2024</p>
        </aside>
-      <main>
+      {/* <main> 
+          { 
+            userList.map((userItem, i)=> <Card user={userItem} key={i}/>)
+          } 
+       
+
+      </main> */}
+      <div className="data-grid">
+     
+      {
+       
+      <table>
+        <tr>
+          <th>Ime i prezime</th> <th>Gender: </th> <th>Age: </th> <th>Hobby</th><th>Phone number</th>
+        </tr>
+        {isIncluded && <h1 style={{padding:10}}>No Item added yet</h1>}
         {
-          userList.map(userItem=>(
-            <Card user={userItem} key={userItem && userItem.ime}/>
-          ))
+        userList.map(userItem=>(
+        <tr><td>{userItem.ime} {userItem.prezime}</td><td>{userItem.gender}</td><td>{userItem.age}</td><td>{userItem.hobby}</td><td>{userItem.number}</td></tr>
+        ))
         }
-        
-         
-      </main>
+      </table>
+        }
+
+      </div>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 const Card = ({user}) => {
-    const {ime,prezime,gender,age,hobby} = user;
+    const {ime,prezime,gender,age,hobby,number} = user;
     return (  
         <div className="card" key={ime}>
             <p>{ime} {prezime}</p>
             <p>gender: {gender}</p>
             <p>Age: {age}</p>
-            <p>hobby: {hobby}</p>
+            <p>hobby: {hobby}</p> 
+            <p>hobby: {number}</p>
         </div>
     );
 }
